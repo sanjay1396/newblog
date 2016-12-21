32c5d87374b45856276281a58291636e5c466c56
Sanjay <sanjay@hitwicket.com>
2016-12-21 17:01:02 +0530

    Commit

--- protected/controllers/CommentController.php
+++ protected/controllers/CommentController.php
@@ -18,25 +18,32 @@ class CommentController extends Controller {
 
     public function actionCount($id){
         $counts = Comment::model()->findAllByAttributes(array('post_id'=>$id));
-        $users_data = array();
-        foreach ($counts as $count) {
-           $users_data[] = array('user_id'=>$count->user_id,'user_name'=>$count->user->name);
+        if(!$counts){
+            $this->Error('The id you have entered is invalid');
         }
-       echo "No. of Comments = ".count($counts)."  "."<br>";
-       echo CJSON::encode(array('status'=>'SUCCESS',
+        else {
+            $users_data = array();
+            foreach ($counts as $count) {
+            $users_data[] = array('user_id'=>$count->user_id,'user_name'=>$count->user->name);
+            }
+            $this->Success(array('no_of_comments'=>count($counts),'users_data'=>$users_data));
+        }
+    }
 
-               'users_data'=>$users_data,
-           ));
-   }
-
-   public function actionTopComments($id){
-
-       $comments = Comment::model()->findAll(array('condition'=>"post_id = :post_id", 'params'=>array('post_id'=>$id), 'order'=>'created_at DESC', 'limit'=>5));
-       $comments_data = array();
-       foreach($comments as $comment){
-           $comments_data[] = array('user_name'=>$comment->user->name, 'content'=>$comment->content);
-       }
-       echo CJSON::encode(array('status'=>'SUCCESS', 'Comments_information'=>$comments_data));
-   }
+    public function actionTopComments($id){
+        $comments = Comment::model()->findAll(array('condition'=>"post_id = :post_id", 'params'=>array('post_id'=>$id), 'order'=>'created_at DESC', 'limit'=>5));
+        
+        if(!$comments){
+            $this->Error('The id you have entered is invalid');
+        }
+        
+        else {
+            $comments_data = array();
+            foreach($comments as $comment){
+            $comments_data[] = array('user_name'=>$comment->user->name, 'content'=>$comment->content);
+            }
+            $this->Success(array('comments_information'=>$comments_data));
+        }
+    }
 
 }
--- protected/controllers/LikeController.php
+++ protected/controllers/LikeController.php
@@ -5,8 +5,8 @@ class LikeController extends Controller {
             if(isset($_POST['Like'])) {
             $like = Like::create($_POST['Like']);
             if(!$like->errors) {
-                $this->Success(array());
-                echo "Liked Successfully";
+                $this->Success(array('Liked Successfully'));
+                //echo "Liked Successfully";
             } else {
                 $this->Error($this->ModelErrorMessage($like));
             }
@@ -14,4 +14,20 @@ class LikeController extends Controller {
             $this->Error('Please Send the like data first...!!');
         }
     }
+
+    public function actionCount($id){
+        $counts = Like::model()->findAllByAttributes(array('post_id'=>$id));
+        if(!$counts){
+            $this->Error('The id you have entered is invalid');
+        }
+        else {
+            $users_data = array();
+        foreach ($counts as $count) {
+           $users_data[] = array('user_id'=>$count->user_id,'user_name'=>$count->user->name);
+        }
+       //echo "No. of Likes = ".count($counts)."  "."<br>";
+       //echo CJSON::encode(array('status'=>'SUCCESS','users_data'=>$users_data,));
+        $this->Success(array('no_of_likes'=>count($counts),'users_data'=>$users_data));
+        }
+   }
 }
